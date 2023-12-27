041df2c139f441bea7b974c8cdfbb4b6d8993149
Victoriia-Ripka <vikaripka@gmail.com>
2023-12-27 18:00:03 +0100

    orders

--- src/app/sections/orders/orders.component.ts
+++ src/app/sections/orders/orders.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent {
+  ordersList: Array<any> = [
+    {
+      id: 7643980998990,
+      date: 'October 8,2021',
+      status: 'Delivered',
+      total: 105,
+    },
+    {
+      id: 943980998990,
+      date: 'October 8,2021',
+      status: 'Processing',
+      total: 100,
+    },
+    {
+      id: 879980998990,
+      date: 'October 8,2020',
+      status: 'Delivered',
+      total: 65,
+    }]
+  ordersCount: number = this.ordersList.length
 
 }
