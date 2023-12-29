f9f427babde768e9222215b104705ceeb146cbcc
Victoriia-Ripka <vikaripka@gmail.com>
2023-12-30 00:02:43 +0100

    shopping cart

--- src/app/sections/shopping-inforation/shopping-inforation.component.ts
+++ src/app/sections/shopping-inforation/shopping-inforation.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-inforation.component.scss']
 })
 export class ShoppingInforationComponent {
+  items: Array<any> = [
+    {
+      title: "Dutas",
+      dose: "10 pills x 0.5mg",
+      amount: "1",
+      price: "23.30",
+      cost: "23.30"
+
+    }
+  ]
+  totalPrice: string = '23.30'
+  step: 'first' | 'second' | 'third' = 'first'
+
+
+  clickToSecondStep() {
+    this.step = 'second'
+  }
 
 }
