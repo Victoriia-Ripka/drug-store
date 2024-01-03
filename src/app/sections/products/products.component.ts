e0ce10efbf99c7960a6b5b1604fd34333106412d
Victoriia-Ripka <vikaripka@gmail.com>
2024-01-03 23:44:09 +0100

    sort products by option done

--- src/app/sections/products/products.component.ts
+++ src/app/sections/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from 'src/app/services';
 import { categoriesList } from 'src/app/data';
-// import { PaginationControlsDirective } from 'ngx-pagination';
 
 @Component({
   selector: 'products',
@@ -13,6 +12,7 @@ export class ProductsComponent {
   products: any[] = []
   producrsAmount: number = 0
   selectedCategory: string = 'All Products'
+  selectedSortOption: string = 'all'
 
   pageSize: number = 9
   currentPage: number = 1
@@ -24,19 +24,39 @@ export class ProductsComponent {
   }
 
   selectCategory(category: string) {
-    this.selectedCategory = category;
-    this.loadData();
+    this.selectedCategory = category
+    this.loadData()
+  }
+
+  selectSortOption(option: string) {
+    this.selectedSortOption = option
+    this.loadData()
   }
 
   loadData() {
     this.dataService.getData().subscribe((data) => {
       if (this.selectedCategory !== 'All Products') {
-        this.products = data.filter(item => item.category === this.selectedCategory);
-      } else {
-        this.products = data;
+        this.products = data.filter(item => item.category === this.selectedCategory)
       }
 
-      this.producrsAmount = this.products.length;
+      switch (this.selectedSortOption) {
+        // case 'average':
+          // what is avarage
+          // data = data.sort((a, b) => a.price - b.price)
+          // break
+        case 'popular':
+          data = data.sort((a, b) => a.popular - b.popular)
+          break
+        case 'cheapest':
+          data = data.sort((a, b) => a.price - b.price)
+          break
+        case 'expensive':
+          data = data.sort((a, b) => b.price - a.price)
+          break
+      }
+
+      this.products = data
+      this.producrsAmount = this.products.length
     });
   }
 }
